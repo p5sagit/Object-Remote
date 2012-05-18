beaee5a1ec2d8d70222163de995aa99adff0f052
Matt S Trout <mst@shadowcat.co.uk>
2012-05-18 15:12:28 +0000

    search sensibly for the object-remote-node script

--- lib/Object/Remote/Connector/Local.pm
+++ lib/Object/Remote/Connector/Local.pm
@@ -6,7 +6,11 @@ use Moo;
 with 'Object::Remote::Role::Connector';
 
 sub _open2_for {
-  # XXX bin/ is wrong but meh, fix later
+  my $open_this = (
+    -d 't' && -e 'bin/object-remote-node'
+      ? 'bin/object-remote-node'
+      : 'object-remote-node'
+  );
   my $pid = open2(my $its_stdout, my $its_stdin, 'bin/object-remote-node')
     or die "Couldn't start local node: $!";
   return ($its_stdin, $its_stdout, $pid);
