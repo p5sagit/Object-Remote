ffc818dd92a3e0434bbee8d7843fcabb9e0bbb44
Matt S Trout <mst@shadowcat.co.uk>
2012-07-23 17:18:37 +0000

    bump version

--- lib/Object/Remote.pm
+++ lib/Object/Remote.pm
@@ -4,7 +4,7 @@ use Object::Remote::MiniLoop;
 use Object::Remote::Handle;
 use Module::Runtime qw(use_module);
 
-our $VERSION = '0.002001'; # 0.2.1
+our $VERSION = '0.002002'; # 0.2.2
 
 sub new::on {
   my ($class, $on, @args) = @_;
