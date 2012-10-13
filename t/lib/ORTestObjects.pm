6b353e966ee45889021503f14e4b8814c7a8be58
Jess Robinson <castaway@desert-island.me.uk>
2012-10-13 19:32:16 +0100

    Test attempt for passing objects around via O::R

---
+++ t/lib/ORTestObjects.pm
@@ -0,0 +1,30 @@
+package ORTestObjects;
+
+use Moo;
+
+has name => (is => 'rwp', default => sub { 'Fred' });
+
+sub same_name {
+    my ($self, $other) =  @_;
+
+    return $self->name eq $other->name;
+}
+
+sub give_back {
+    my ($self) = @_;
+
+    return $self;
+}
+
+sub takes_object {
+    my ($self, $object) = @_;
+
+    print STDERR ref($object);
+    if($object->isa('ORTestObjects')) {
+        return 1;
+    }
+
+    return 0;
+}
+
+1;
