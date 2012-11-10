81444b66f42d60a0860a6a955bb63ef160e2f98a
Tyler Riddle <t.riddle@shadowcat.co.uk>
2012-11-09 17:40:34 -0800

    add comment about flaw with alarm in node startup; reduce log level of a message

--- lib/Object/Remote/Node.pm
+++ lib/Object/Remote/Node.pm
@@ -18,7 +18,7 @@ sub run {
   my $loop = Object::Remote->current_loop;
   
   $c->on_close->on_ready(sub { 
-    log_info { "Node connection with call handler has closed" };
+    log_debug { "Node connection with call handler has closed" };
     $loop->want_stop 
   });
 
@@ -26,6 +26,10 @@ sub run {
   print { $c->send_to_fh } "Shere\n";
 
   log_debug { "Node is going to start the run loop" };
+  #TODO the alarm should be reset after the run loop starts
+  #at a minimum - the remote side node should probably send
+  #a command that clears the alarm in all instances - even 
+  #if the Object::Remote::Watchdog is not being used 
   if ($args{watchdog_timeout}) {
     Object::Remote::WatchDog->new(timeout => $args{watchdog_timeout});
   } else {
