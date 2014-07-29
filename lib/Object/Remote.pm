907c9c830593819ba24a822214ec427f3bc5bfb5
Matt S Trout <mst@shadowcat.co.uk>
2014-07-29 03:17:22 +0000

    bump version

--- lib/Object/Remote.pm
+++ lib/Object/Remote.pm
@@ -5,7 +5,7 @@ use Object::Remote::Handle;
 use Object::Remote::Logging qw( :log );
 use Module::Runtime qw(use_module);
 
-our $VERSION = '0.003002'; # 0.3.2
+our $VERSION = '0.003003'; # 0.3.3
 
 sub new::on {
   my ($class, $on, @args) = @_;
